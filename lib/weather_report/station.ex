aa56ea4d5f0d9ceeec04710475cad33b9f752e86
Dan Janowski <danj@3skel.com>
2023-04-11 17:30:25 -0400

    fix station loading
    move station loading to Station
    remove supervisor.ex, replaced earlier

--- lib/weather_report/station.ex
+++ lib/weather_report/station.ex
@@ -3,7 +3,7 @@ defmodule WeatherReport.Station do
   NOAA observation station.
   """
   
-  import SweetXml
+  import SweetXml, only: [sigil_x: 2]
   @xmap [
     station_id: ~x"//station/station_id/text()"s,
     state: ~x"//station/state/text()"s,
@@ -40,8 +40,8 @@ defmodule WeatherReport.Station do
   @doc """
   Parses an xml document into a station.
   """
-  @spec parse(String.t) :: t
-  def parse(doc) do
+  @spec parse_station(String.t()) :: t
+  def parse_station(doc) do
     doc
     |> SweetXml.stream_tags(:station)
     |> Stream.map(&station_xmapper/1)
@@ -58,5 +58,35 @@ defmodule WeatherReport.Station do
   defp parse_coordinate(coordinate) do
     {float, _} = Float.parse(coordinate)
     float
+  end
+
+  @station_list "https://w1.weather.gov/xml/current_obs/index.xml"
+
+  def station_list do
+    with {:ok, %HTTPoison.AsyncResponse{id: ref}} <-
+           HTTPoison.get(@station_list, %{}, stream_to: self),
+         {:ok, doc} <- receive_async(ref, ""),
+         do: parse_station(doc)
+  end
+
+  defp receive_async(ref, doc) do
+    alias HTTPoison, as: HP
+
+    receive do
+      %HP.AsyncStatus{code: code, id: ^ref} when code in 200..399 ->
+        receive_async(ref, doc)
+
+      %HP.AsyncStatus{code: code, id: ^ref} ->
+        {:error, "Unable to fetch station list, http #{code}"}
+
+      %HP.AsyncHeaders{id: ^ref} ->
+        receive_async(ref, doc)
+
+      %HP.AsyncChunk{chunk: chunk, id: ^ref} ->
+        receive_async(ref, doc <> chunk)
+
+      %HP.AsyncEnd{id: ^ref} ->
+        {:ok, doc}
+    end
   end
 end
